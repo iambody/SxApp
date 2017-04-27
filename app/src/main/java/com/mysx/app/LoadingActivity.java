a9378d09ef3ddcbc6662d3a7848c1a7c3c49fd06
wangyongkui <beist@sohu.com>
2017-04-27 10:21:04 +0800

    Revert "第四次提交11"
    This reverts commit 95e7a5f3cdf1755ebcc088dad3a7320a52c5abdc.

--- app/src/main/java/com/mysx/app/LoadingActivity.java
+++ app/src/main/java/com/mysx/app/LoadingActivity.java
@@ -9,6 +9,5 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-        //开始是是是
     }
 }
